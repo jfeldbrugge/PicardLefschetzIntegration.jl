0c58a010da88f6b600eeb69705ac7cd8d05a5895
Documenter.jl <documenter@juliadocs.github.io>
2026-06-01 22:42:36 +0000

    build based on 1975b2d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"category":"section","location":"#PicardLefschetzIntegration","page":"Home","text":"Documentation for PicardLefschetzIntegration.\n\n","title":"PicardLefschetzIntegration"}]
+}
