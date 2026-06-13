301e4e5f886cb2857af8c72aadb300d713beea35
Job Feldbrugge <14946916+jfeldbrugge@users.noreply.github.com>
2026-06-13 06:49:46 +0100

    Start runtest.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,10 @@ using PicardLefschetzIntegration
 using Test
 
 @testset "PicardLefschetzIntegration.jl" begin
-    # Write your tests here.
+    pars = parameters(δ = 0.5, τ = -10., ϵ = 0.1, N = 20, n = 5, dim = 1)
+
+    S(p) = p[1]^2
+    thim = initialGrid([-4], [4], pars)
+    flow(S, thim, pars)
+    @test abs.(PL(S, thim, pars) - (1+im) * sqrt(π / 2)) < 1e-4
 end
